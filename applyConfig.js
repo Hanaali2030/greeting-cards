bbd8574a95e6b40ded8f790211fa8758e92af95e
eid-cards <cards_paltform@hotmail.com>
2025-03-28 17:43:57 +0300

    Update applyConfig.js

--- applyConfig.js
+++ applyConfig.js
@@ -45,11 +45,3 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-// تأكيد ظهور الحقوق حتى لو تأخر تحميل التذييل
-const waitForFooter = setInterval(() => {
-  const footerEl = document.getElementById("footerText");
-  if (footerEl) {
-    footerEl.innerHTML = `© ${config.footerYear} جميع الحقوق محفوظة لـ ${config.footerEntityName} - <strong>تطوير: أبو تميم</strong>`;
-    clearInterval(waitForFooter);
-  }
-}, 100);
