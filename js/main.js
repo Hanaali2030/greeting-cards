2986843824a4f83a9c9d677c38b458bfe415752b
greeting-cards <cards_paltform@hotmail.com>
2025-04-01 09:24:46 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,14 +10,7 @@ function generateImage(designNumber) {
     canvas.width = img.width;
     canvas.height = img.height;
 
-    const designSettings = {
-      1: { fontSize: 35, fontColor: '#520205', x: img.width / 2, y: 1380 },
-      2: { fontSize: 38, fontColor: '#2f2f2f', x: 315, y: 1310 },
-      3: { fontSize: 55, fontColor: '#333333', x: img.width / 2, y: 1365 },
-      4: { fontSize: 40, fontColor: '#000000', x: 300, y: 1300 },
-      5: { fontSize: 38, fontColor: '#0f6f2f', x: img.width / 2, y: 1065 },
-      6: { fontSize: 30, fontColor: '#4e615f', x: img.width / 2, y: 950 }
-    };
+    const settings = nameSettings[designNumber] || { fontSize: 36, fontColor: '#006699', x: img.width / 2, y: 500 };
 
     const settings = designSettings[designNumber] || { fontSize: 36, fontColor: '#006699', x: img.width / 2, y: 500 };
 
