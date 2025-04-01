59e87cbd8fb9b59db832bd14b2be130485b43bd8
greeting-cards <cards_paltform@hotmail.com>
2025-04-01 09:20:44 +0300

    Create nameSettings.js

---
+++ js/nameSettings.js
@@ -0,0 +1,9 @@
+// nameSettings.js
+const nameSettings = {
+      1: { fontSize: 35, fontColor: '#520205', x: 300, y: 1380 },
+      2: { fontSize: 38, fontColor: '#2f2f2f', x: 315, y: 1310 },
+      3: { fontSize: 55, fontColor: '#333333', x: 300, y: 1365 },
+      4: { fontSize: 40, fontColor: '#000000', x: 300, y: 1300 },
+      5: { fontSize: 38, fontColor: '#0f6f2f', x: 300, y: 1065 },
+      6: { fontSize: 30, fontColor: '#4e615f', x: 300, y: 950 }
+};
