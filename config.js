09b28cba82729b7353f2aa1f01424c9db25baf4e
greeting-cards <cards_paltform@hotmail.com>
2025-04-01 06:32:22 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,32 +1,17 @@
 const config = {
-  // 1) نوع الشعار: "text" أو "image"
-  logoType: "image",
-
-  // 2) بيانات الشعار:
-  logoImageHeight: 60, // بكسل
-  logoText: "جهة افتراضية",
-  logoTextColor: "#006699",
-  logoTextFont: "Tajawal",
-  logoTextSize: 24,
-
-  // 3) نص المناسبة (يتم دمجه مع "اختر بطاقتك ل...")
-  occasionText: "عيد الفطر",
-
-  // 4) العنوان في وسم <title>
-  pageTitle: "بطاقات العيد - كل عام وأنتم بخير",
-
-  // 5) الألوان العامة
-  backgroundColor: "#f9f9f9",
-  buttonColor: "#006699",
-
-  // 6) الحقوق في التذييل
-  footerEntityName: "جمعية فزعة للإغاثة بالزلفي", // فقط الاسم المتغير
-  footerYear: "2025",
-
-  // 7) إعدادات عداد الزوار
-  visitorBadgeBase: "eid99999-cards", // يُستخدم مع اسم الصفحة
-  enableVisitorBadge: true,
-
-  // 8) عدد التصاميم المتوفرة
-  designCount: 6 // اختر من 1 الى 6
+  "logoType": "image",
+  "logoImageHeight": 60,
+  "logoText": "جهة افتراضية",
+  "logoTextColor": "#006699",
+  "logoTextFont": "Tajawal",
+  "logoTextSize": 24,
+  "occasionText": "التخرج",
+  "pageTitle": "بطاقات التخرج - ألف مبروك",
+  "backgroundColor": "#f9f9f9",
+  "buttonColor": "#006699",
+  "footerEntityName": "مدرسة عمر بن الخطاب",
+  "footerYear": "2030",
+  "visitorBadgeBase": "eid99999114-cards",
+  "enableVisitorBadge": true,
+  "designCount": 4
 };
