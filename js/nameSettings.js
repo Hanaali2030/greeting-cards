960da4b6960101fb9cbcca9146fb764d3cba9807
greeting-cards <cards_paltform@hotmail.com>
2025-04-01 11:12:47 +0300

    Add files via upload

--- js/nameSettings.js
+++ js/nameSettings.js
@@ -1,26 +1,38 @@
 const nameSettings = {
   "1": {
     "fontSize": 50,
-    "fontColor": "#f40b0b",
-    "x": 449,
-    "y": 74
+    "fontColor": "#f5f4f0",
+    "x": 539,
+    "y": 1578
   },
   "2": {
-    "fontSize": 16,
-    "fontColor": "#5e1111",
-    "x": 174,
-    "y": 261
+    "fontSize": 40,
+    "fontColor": "#611132",
+    "x": 548,
+    "y": 1601
   },
   "3": {
     "fontSize": 30,
-    "fontColor": "#000000",
-    "x": 579,
-    "y": 1196
+    "fontColor": "#a42b29",
+    "x": 425,
+    "y": 1004
   },
   "4": {
-    "fontSize": 12,
-    "fontColor": "#8f6435",
-    "x": 732,
-    "y": 199
+    "fontSize": 50,
+    "fontColor": "#fbfbfb",
+    "x": 632,
+    "y": 1450
+  },
+  "5": {
+    "fontSize": 30,
+    "fontColor": "#373743",
+    "x": 555,
+    "y": 881
+  },
+  "6": {
+    "fontSize": 45,
+    "fontColor": "#924e27",
+    "x": 561,
+    "y": 1130
   }
 };
