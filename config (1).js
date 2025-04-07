70af044a5f52a8b1402dfebbef20965ee7fb3dbd
Hanaali2030 <deshanaa147@gmail.com>
2025-04-07 16:04:25 +0300

    Add files via upload

---
+++ config (1).js
@@ -0,0 +1,19 @@
+const config = {
+  "logoType": "image",
+  "logoImageShape": "fixed",
+  "logoImageHeight": 180,
+  "logoImageWidthPercent": 50,
+  "logoText": "منصة بطاقات التهنئة",
+  "logoTextColor": "#006699",
+  "logoTextFont": "Tajawal",
+  "logoTextSize": 24,
+  "occasionText": "عيد الفطر",
+  "pageTitle": "بطاقات العيد - كل عام وأنتم بخير",
+  "backgroundColor": "#f9f9f9",
+  "buttonColor": "#006699",
+  "footerEntityName": "منصة بطاقات التهنئة",
+  "footerYear": "2025",
+  "visitorBadgeBase": "eid123456-cards",
+  "enableVisitorBadge": true,
+  "designCount": 6
+};
